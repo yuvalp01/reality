6fc7397fbcfedc281c025ccdfc8a4b8c9b279484
Yuval <yuvalp01@gmail.com>
2019-08-21 09:50:10 +0200

    enable client side sorting

--- Nadlan/ClientApp/src/app/fetch-data/fetch-transactions.component.ts
+++ Nadlan/ClientApp/src/app/fetch-data/fetch-transactions.component.ts
@@ -1,39 +1,62 @@
-import {Component, OnInit, Output} from '@angular/core';
+import { Component, OnInit, Output, ViewChild } from '@angular/core';
 import { ITransaction } from '../shared/models';
 import { TransactionService } from '../services/transaction.service';
 import { resource } from 'selenium-webdriver/http';
 import { AddTransactionComponent } from '../forms/add-transaction.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSort, MatTableDataSource } from '@angular/material';
 import { subscribeOn } from 'rxjs/operators';
 
 @Component({
-    templateUrl:'./fetch-transactions.component.html',
-    styles:['table{width:100%}']
+  templateUrl: './fetch-transactions.component.html',
+  styles: ['table{width:100%}']
 })
-export class TransactionListComponent implements OnInit
-{
-    displayedColumns: string[] = ['date','amount', 'apartmentId','accountId','isPurchaseCost','comments'];
-    transactions: ITransaction[];
-    constructor (private transactionService: TransactionService, private dialog: MatDialog)
-    {
-    }
-    ngOnInit(): void {
-        this.transactionService.getTransactions().subscribe(result=>this.transactions=result, error=>console.error(error));
-    }
-    //dialogRef:any;
-    openDialog()
-    {
-        
-         let dialogRef = this.dialog.open(AddTransactionComponent, {
-            height: '600px',
-            width: '500px',
-          }).afterClosed().subscribe(item=>{
-            this.transactionService.getTransactions().subscribe(result=>this.transactions=result, error=>console.error(error));
-          });
+export class TransactionListComponent implements OnInit {
+  displayedColumns: string[] = ['id','date', 'amount', 'apartmentId', 'accountId', 'isPurchaseCost', 'comments'];
+  //transactions: ITransaction[];
+  dataSource = new MatTableDataSource<ITransaction>();
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  constructor(private transactionService: TransactionService, private dialog: MatDialog) {
+  }
+  ngOnInit(): void {
+    this.transactionService.getTransactions().subscribe(result => {
 
-        //   this.dialogRef.componentInstance.test.subscribe(()=>{
-        //     console.log('test');
-        //   });
-    }
+      this.dataSource.data = result as ITransaction[];
+      //this.transactions = result;
+      this.dataSource = new MatTableDataSource(result);
+      this.dataSource.sort = this.sort;
+      //this.dataSource.filter = 'x';
+
+
+      this.dataSource.filterPredicate = function (data, filter: string): boolean {
+        return data.apartmentId.toString() === filter;
+      };
+
+
+      }, error => console.error(error));
+}
+//dialogRef:any;
+openDialog()
+{
+
+  let dialogRef = this.dialog.open(AddTransactionComponent, {
+    height: '600px',
+    width: '500px',
+  }).afterClosed().subscribe(item => {
+
+    this.transactionService.getTransactions().subscribe(result => {
+
+      this.dataSource.data = result as ITransaction[];
+      //this.transactions = result;
+      this.dataSource = new MatTableDataSource(result);
+      this.dataSource.sort = this.sort
+    }, error => console.error(error));
+
+    //this.transactionService.getTransactions().subscribe(result=>this.transactions=result, error=>console.error(error));
+  });
+}
+
+ngAfterViewInit(): void {
+  this.dataSource.sort = this.sort;
+}
 
 }
