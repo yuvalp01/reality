26c14db90770924d7f2c2a444b178a53089143b9
Yuval <yuvalp01@gmail.com>
2020-01-08 11:06:12 +0200

    Improve transaction view filter

--- Nadlan/ClientApp/src/app/transactions/fetch-transactions.component.ts
+++ Nadlan/ClientApp/src/app/transactions/fetch-transactions.component.ts
@@ -3,17 +3,17 @@ import { ITransaction } from '../models';
 import { TransactionService } from '../services/transaction.service';
 import { MatDialog, MatSort, MatTableDataSource } from '@angular/material';
 import { TransactionFormComponent } from './transaction-form/transaction-form.component';
-import { ExpensesService } from '../services/expenses.service';
+import { element } from 'protractor';
 
 @Component({
   templateUrl: './fetch-transactions.component.html',
-  //styles: ['table{width:100%}']
   styleUrls: ['./fetch-transactions.component.css']
 })
 export class TransactionListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'date', 'apartmentId','isPurchaseCost', 'accountId', 'amount',  'comments', 'actions'];
+  displayedColumns: string[] = ['id', 'date', 'apartmentId', 'isPurchaseCost', 'accountId', 'amount', 'comments', 'actions'];
   dataSource = new MatTableDataSource<ITransaction>();
-
+  showConfirmedOnly: boolean = true;
+  allData: ITransaction[];
   @ViewChild(MatSort, { static: true }) sort: MatSort;
   selectedApartment: any;
   constructor(
@@ -22,25 +22,57 @@ export class TransactionListComponent implements OnInit {
   }
   ngOnInit(): void {
     this.transactionService.getTransactions().subscribe(result => {
-
-      this.dataSource.data = result as ITransaction[];
+      this.allData = result as ITransaction[];
+      this.dataSource.data = this.allData;
       this.dataSource.sort = this.sort;
-      this.dataSource.filterPredicate = function (data, filter: string): boolean {
-        return data.apartmentAddress.toLowerCase().includes(filter);
-      };
     }, error => console.error(error));
   }
 
   refreshData() {
     //refresh tables
     this.transactionService.getTransactions().subscribe(result => {
-      this.dataSource.data = result as ITransaction[];
+      this.allData = result as ITransaction[];
+      this.filterIsConfirmed();
+      //this.dataSource.data = this.allData;
       this.dataSource.sort = this.sort;
-      this.dataSource.filterPredicate = function (data, filter: string): boolean {
-        return data.apartmentAddress.toLowerCase().includes(filter);
-      };
+      //this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      //  return data.apartmentAddress.toLowerCase().includes(filter);
+      //};
     }, error => console.error(error));
   }
+
+  showUnconfirmed() {
+    this.dataSource.data = this.allData.filter(a => !a.isConfirmed);
+  }
+  showAll() {
+    this.dataSource.data = this.allData;
+  }
+
+  hideShowUnconfirmed(val: any) {
+    this.showConfirmedOnly = val.checked;
+    this.filterIsConfirmed();
+  }
+
+  filterIsConfirmed() {
+    if (this.showConfirmedOnly) {
+      this.dataSource.data = this.allData.filter(a => !a.isConfirmed);
+    }
+    else {
+      this.dataSource.data = this.allData;
+    }
+  }
+
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+
+  }
+  doFilter(value: string) {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+
+
+
 
   openForm(_transactionId: number) {
     let dialogRef = this.dialog.open(TransactionFormComponent, {
@@ -76,14 +108,9 @@ export class TransactionListComponent implements OnInit {
 
   }
 
-  doFilter(value: string) {
-    this.dataSource.filter = value.trim().toLocaleLowerCase();
-  }
 
-  filter() {
-    this.dataSource.filter = this.selectedApartment.trim().toLocaleLowerCase();
 
-  }
+
   public isPositive(value: number): boolean {
     if (value >= 0) {
       return true;
@@ -92,22 +119,79 @@ export class TransactionListComponent implements OnInit {
   }
 
 }
-  //openDialog()
-  //{
 
-  //  let dialogRef = this.dialog.open(AddTransactionComponent, {
-  //    height: '600px',
-  //    width: '500px',
-  //    data: {type:'full'}
-  //  }).afterClosed().subscribe(item => {
 
-  //    this.transactionService.getTransactions().subscribe(result => {
 
-  //      this.dataSource.data = result as ITransaction[];
-  //      this.dataSource = new MatTableDataSource(result);
-  //      this.dataSource.sort = this.sort
-  //      //this.filter();
-  //    }, error => console.error(error));
 
-  //  });
+  //showUnconfirmed() {
+  //  //console.log(value);
+  //  //console.log(value.checked);
+  //  this.dataSource.filter = value.checked.toString();
+  //}
+      //this.dataSource.filterPredicate == null;
+
+      //this.dataSource.filterPredicate =
+      //  (data: ITransaction, filtersJson: string) => {
+      //    const matchFilter = [];
+      //    const filters = JSON.parse(filtersJson);
+
+      //    filters.forEach(filter => {
+      //      let val = data[filter.id] === null ? '' : data[filter.id];
+      //      let filterVal = filter.valuetoLowerCase();
+      //      val = val.toLowerCase();
+      //      matchFilter.push(val.includes(filterVal));
+      //    });
+      //    return matchFilter.every(Boolean);
+      //  };
+
+      //this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      //  let yyy = data.isConfirmed.toString();
+      //  return yyy != filter;
+      //  //let xxx = data.isConfirmed.includes(filter);
+      //  return data.apartmentAddress.toLowerCase().includes(filter);
+      //};
+    //this.dataSource.filterPredicate = function (data, filter: string): boolean {
+    //  let yyy = data.isConfirmed.toString();
+    //  return yyy != filter;
+    //  //let xxx = data.isConfirmed.includes(filter);
+    //  return data.apartmentAddress.toLowerCase().includes(filter);
+    //};
+
+
+    //this.dataSource.filterPredicate = (d: ITransaction, filter: string) => {
+    //  const textToSearch = d[column] && d[column].toLowerCase() || '';
+    //  return textToSearch.indexOf(filter) !== -1;
+    //};
+
+  //  this.dataSource.filterPredicate == null;
+  //  this.dataSource.filter = "";
+
+    //const tableFilters = [];
+    //tableFilters.push({
+    //  id: 'apartmentAddress',
+    //  value: filterValue
+    //},
+    //  //{
+    //  //  id: 'comments',
+    //  //  value: filterValue
+    //  //}
+
+    //);
+    //this.dataSource.filter = JSON.stringify(tableFilters);
+  //customFilterPredicate() {
+  //  this.dataSource.filterPredicate = function (data, filter: string): boolean {
+  //    let xxx = data.isConfirmed === filter;
+  //    return data.apartmentAddress.toLowerCase().includes(filter);
+  //  };
+  //}
+
+
+  //xxx(val: boolean) {
+  //  console.log(val);
+  //  //this.dataSource.filter = value.trim().toLocaleLowerCase();
+
+  //}
+  //onChangeYuv(val: any) {
+  //  console.log(val);
+  //  console.log(val.checked);
   //}
